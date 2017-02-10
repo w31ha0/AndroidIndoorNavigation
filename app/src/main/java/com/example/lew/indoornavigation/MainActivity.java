45e8b0cab7d0ce25e38f3cd80f89c2ef4b801acd
Lew Wei Hao <Lew Wei Hao>
2017-02-10 13:36:47 +0000

    Basic algorithm for step detection

--- app/src/main/java/com/example/lew/indoornavigation/MainActivity.java
+++ app/src/main/java/com/example/lew/indoornavigation/MainActivity.java
@@ -16,13 +16,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView acc_y;
     private TextView acc_z;
     private TextView status;
-
+    private TextView steps;
 
     private SensorManager sensorManager;
     private Sensor sensorAccel;
     private Context context;
+    private int noOfSteps = 0;
 
     private boolean isRunning = false;
+    private final float sampling_period = 300000000f;
+    private final float acc_sensitivity = 1.5f;
+    private long lastTime = 0;
+    private float prevAcc = 0f;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +39,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         acc_y = (TextView)findViewById(R.id.acc_y);
         acc_z = (TextView)findViewById(R.id.acc_z);
         status = (TextView)findViewById(R.id.status);
+        steps = (TextView)findViewById(R.id.steps);
 
         if (isRunning || !IsKitKatWithStepCounter(context.getPackageManager()))
             //status.setText("This phone does not have sensors");
@@ -44,13 +51,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent e) {
-        status.setText("Step Detected!");
+        long currTime = e.timestamp;
         switch (e.sensor.getType()) {
             case Sensor.TYPE_ACCELEROMETER:
+                System.out.println("Current is "+currTime);
+                System.out.println("Last is "+lastTime);
+                System.out.println("Diff is "+(currTime - lastTime));
+
                 float[] acceleration = e.values;
                 acc_x.setText("X:"+String.valueOf(acceleration[0]));
                 acc_y.setText("Y:"+String.valueOf(acceleration[1]));
                 acc_z.setText("Z:"+String.valueOf(acceleration[2]));
+
+                if (currTime - lastTime > sampling_period){
+                    float acc_diff = acceleration[2] - prevAcc ;
+                    status.setText("Difference is "+acc_diff);
+                    if (acc_diff > acc_sensitivity) {
+                        noOfSteps++;
+                        steps.setText(String.valueOf(noOfSteps));
+                    }
+                    prevAcc = acceleration[2];
+                    lastTime = e.timestamp;
+                }
                 break;
             default:
         }
@@ -84,7 +106,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        UnregisterListeners ();
+        UnregisterListeners();
         isRunning = false;
     }
 
